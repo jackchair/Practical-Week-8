9737e235d2fc564314347d5f54f9ce2fa61802fe
jackchair <68882404+jackchair@users.noreply.github.com>
2021-10-06 16:03:55 +0800

    Implement models and retexture the player model

--- Week 8/Assets/Scripts/Coin.cs
+++ Week 8/Assets/Scripts/Coin.cs
@@ -7,7 +7,7 @@ public class Coin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Renderer>().material.color=Color.yellow;
+        
     }
 
     // Update is called once per frame
